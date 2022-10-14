0b950ae09095c2852ab3d6c790a94888c9eccfe7
gabriele16 <gabrielet85@gmail.com>
2022-10-14 15:12:31 +0200

    first commit!

---
+++ install_interface.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+echo "Installing requirements (pytorch 1.12)"
+#pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
+pip3 install -r requirements.txt
+echo "Build interface"
+cd neural_net && mkdir -p build \
+&& cd build && cmake  -DCMAKE_PREFIX_PATH=`python3 -c 'import torch;print(torch.utils.cmake_prefix_path)'` .. && cmake --build . --config Release
