790d10430c710c13a7ea472ff83ac31af6957171
gabriele16 <gabrielet85@gmail.com>
2022-10-14 18:27:28 +0200

    made small neural network with PyTorch C++ API

--- neural_net/neural_net.cpp
+++ neural_net/neural_net.cpp
@@ -1,7 +1,33 @@
 #include <torch/torch.h>
 #include <iostream>
 
-int main() {
-  torch::Tensor tensor = torch::eye(3);
-  std::cout << tensor << std::endl;
+struct Net : torch::nn::Module
+{
+  Net(int64_t N, int64_t M)
+      : linear(register_module("linear", torch::nn::Linear(N, M)))
+  {
+    another_bias = register_parameter("b", torch::randn(M));
+  }
+  torch::Tensor forward(torch::Tensor input)
+  {
+    return linear(input) + another_bias;
+  }
+  torch::nn::Linear linear;
+  torch::Tensor another_bias;
+};
+
+int main()
+{
+  // initialize neural net
+  Net net(4, 5);
+  // loop over the parameters using iterator
+  std::cout << "Initialize NN and loop over parameters using iterator" << std::endl;
+  for (const auto &pair : net.named_parameters())
+  {
+    std::cout << pair.key() << ": " << pair.value() << std::endl;
+  }
+
+  // do a forward pass
+  std::cout << "Do a forward pass" << std::endl;
+  std::cout << net.forward(torch::ones({2, 4})) << std::endl;
 }
