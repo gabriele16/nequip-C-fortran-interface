ff1f89d27e83b76d7bced67cbd098e30eb733ca2
gabriele16 <gabrielet85@gmail.com>
2022-10-14 18:53:58 +0200

    basic example of neural net in C++ with submodules

---
+++ neural_net/neural_net_submod.cpp
@@ -0,0 +1,33 @@
+#include <torch/torch.h>
+#include <iostream>
+
+struct Net : torch::nn::Module
+{
+  Net(int64_t N, int64_t M)
+      : linear(register_module("linear", torch::nn::Linear(N, M)))
+  {
+    another_bias = register_parameter("b", torch::randn(M));
+  }
+  torch::Tensor forward(torch::Tensor input)
+  {
+    return linear(input) + another_bias;
+  }
+  torch::nn::Linear linear;
+  torch::Tensor another_bias;
+};
+
+int main()
+{
+  // initialize neural net
+  Net net(4, 5);
+  // loop over the parameters using iterator
+  std::cout << "Initialize NN and loop over parameters using iterator with submod" << std::endl;
+  for (const auto &pair : net.named_parameters())
+  {
+    std::cout << pair.key() << ": " << pair.value() << std::endl;
+  }
+
+  // do a forward pass
+  std::cout << "Do a forward pass with submodules" << std::endl;
+  std::cout << net.forward(torch::ones({2, 4})) << std::endl;
+}
